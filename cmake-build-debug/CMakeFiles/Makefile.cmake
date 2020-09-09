0bba226572c8d33e96568060657ef32dbec883a2
jfengan <jfengan@connect.ust.hk>
2020-09-09 18:09:23 +0800

    cmake_config

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -52,5 +52,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/untitled.dir/DependInfo.cmake"
+  "CMakeFiles/professional_c_plus.dir/DependInfo.cmake"
   )
---
+++ cmake-build-debug/CMakeFiles/professional_c_plus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/professional_c_plus.dir/main.cpp.o"
+  "professional_c_plus"
+  "professional_c_plus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/professional_c_plus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
